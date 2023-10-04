7020d3ce2fea6425f11b5f9a53b8437de5b734d6
KyeongMin <93812148+KyeongMin5307@users.noreply.github.com>
2023-10-04 17:16:50 +0900

    Update util.h

--- util/util.h
+++ util/util.h
@@ -1,15 +1,12 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
-static void get_hex(char* dest, const unsigned char* hex, int len)
+static void print_hex(const unsigned char* hex, int len)
 {
 	char hexstr[4];
 	for (const unsigned char* p = hex; p - hex < len; p++)
-	{
-		sprintf(hexstr, "%02X ", *p);
-		strcat(dest, hexstr);
-	}
-	strcat(dest, "\n");
+		printf("%02X ", *p);
+	printf("\n");
 }
 
 #endif
